fafa004f64fcfc7a61d50e42491c6fc40080e13e
mzunigau <mazuga92@gmail.com>
2022-05-17 16:29:18 -0600

    Maquetando el HTML

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit {
-
-  constructor() { }
+  private client: Client;
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    this.client = new Client();
+    this.client.webSocketFactory = () => {
+      return new SockJS('http://localhost:8080/chat-websocket');
+    };
 
+    this.client.onConnect = (frame) => {
+      console.log('Conectados: ' + this.client.connected + ':' + frame);
+    };
+    this.client.activate();
+  }
 }
